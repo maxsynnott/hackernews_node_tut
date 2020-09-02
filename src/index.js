69e68a0e4afacb579c93eeb3fc7b6f50d5c19ac8
maxsynnott <maxryansynnott@gmail.com>
2020-09-02 15:11:40 +0200

    basic graphql server

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+type Query {
+	info: String!
+	feed: [Link!]!
+}
+
+type Link {
+	id: ID!
+	description: String!
+	url: String!
+}
+`;
+
+let links = [
+	{
+		id: "link-0",
+		url: "www.howtographql.com",
+		description: "Fullstack tutorial for GraphQL",
+	},
+];
+
+const resolvers = {
+	Query: {
+		info: () => `A GraphQL server for a Hackernews clone`,
+		feed: () => links,
+	},
+};
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
