f46863f23fe6d5559c1b27dbb703e0138a360a49
maxsynnott <maxryansynnott@gmail.com>
2020-09-03 14:04:12 +0200

    db + prisma setup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { PrismaClient } = require("@prisma/client");
 
-let links = [
-	{
-		id: "link-0",
-		url: "www.howtographql.com",
-		description: "Fullstack tutorial for GraphQL",
-	},
-];
-
-let idCount = links.length;
 const resolvers = {
 	Query: {
 		info: () => `A GraphQL server for a Hackernews clone`,
-		feed: () => links,
+		feed: async (_parent, _args, context) => {
+			return context.prisma.link.findMany();
+		},
 	},
 	Mutation: {
-		post: (parent, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				url: args.url,
-				description: args.description,
-			};
-
-			links.push(link);
+		post: async (_parent, args, context) => {
+			const link = await context.prisma.link.create({
+				data: {
+					url: args.url,
+					description: args.description,
+				},
+			});
 
 			return link;
 		},
 	},
 };
 
+const prisma = new PrismaClient();
+
 const server = new GraphQLServer({
 	typeDefs: "./src/schema.graphql",
 	resolvers,
+	context: {
+		prisma,
+	},
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
